34ed95e4f2186d82feaa1f16c9a665fbf41a6150
Abdrakhman00 <zuparabish@gmail.com>
2025-01-20 11:31:06 +0500

    aa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./components/About/About";
 import Services from "./components/Services/Services";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonials from "./components/Testimonials/Testimonials";
-// import News from "./components/News/News";
+import News from "./components/News/News";
 import Banner from "./components/Banner/Banner";
 import Footer from "./components/Footer/Footer";
 class App extends Component {
@@ -18,7 +18,7 @@ class App extends Component {
         <Services />
         <Portfolio />
         <Testimonials />
-        {/* <News /> */}
+        <News />
         <Banner />
         <Footer />
       </>
