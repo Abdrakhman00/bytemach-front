500e6b607cf8aabc20674c66e6f73247275438d0
Abdrakhman00 <zuparabish@gmail.com>
2025-02-26 11:33:57 +0500

    aaa

--- src/components/RequestForm/RequestForm.jsx
+++ src/components/RequestForm/RequestForm.jsx
@@ -28,69 +28,78 @@ const RequestForm = () => {
   };
 
   return (
-    <div className="form-container">
-      <h2 className="form-title">Оставьте заявку</h2>
-      <form onSubmit={handleSubmit} className="form">
-        <div className="form-row">
+    <div className="form-wrapper">
+      <div className="chat-section">
+        <h2 className="chat-title">Свяжемся?</h2>
+        <p className="chat-description">
+        Готов обсудить ваш проект, 
+        ответить на вопросы или просто пообщаться. Оставьте сообщение через форму обратной связи!
+        </p>
+      </div>
+      <div className="form-container">
+        <h2 className="form-title">Оставьте заявку</h2>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="form-row">
+            <div className="form-group">
+              <label className="form-label"><FaUser /> Имя</label>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Введите ваше имя"
+                value={formData.firstName}
+                onChange={handleChange}
+                className="form-input"
+                required
+              />
+            </div>
+            {/* <div className="form-group">
+              <label className="form-label"><FaUser /> Фамилия</label>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Введите вашу фамилию"
+                value={formData.lastName}
+                onChange={handleChange}
+                className="form-input"
+              />
+            </div> */}
+          </div>
           <div className="form-group">
-            <label className="form-label"><FaUser /> Имя</label>
+            <label className="form-label"><FaPhone /> Телефон</label>
             <input
-              type="text"
-              name="firstName"
-              placeholder="Введите ваше имя"
-              value={formData.firstName}
+              type="tel"
+              name="phone"
+              placeholder="Введите ваш телефон"
+              value={formData.phone}
               onChange={handleChange}
               className="form-input"
               required
             />
           </div>
           <div className="form-group">
-            <label className="form-label"><FaUser /> Фамилия</label>
+            <label className="form-label"><FaEnvelope /> Email</label>
             <input
-              type="text"
-              name="lastName"
-              placeholder="Введите вашу фамилию"
-              value={formData.lastName}
+              type="email"
+              name="email"
+              placeholder="Введите ваш email"
+              value={formData.email}
               onChange={handleChange}
               className="form-input"
             />
           </div>
-        </div>
-        <div className="form-group">
-          <label className="form-label"><FaPhone /> Телефон</label>
-          <input
-            type="tel"
-            name="phone"
-            placeholder="Введите ваш телефон"
-            value={formData.phone}
-            onChange={handleChange}
-            className="form-input"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label className="form-label"><FaEnvelope /> Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Введите ваш email"
-            value={formData.email}
-            onChange={handleChange}
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
-          <label className="form-label"><FaComment /> Сообщение</label>
-          <textarea
-            name="message"
-            placeholder="Введите ваше сообщение"
-            value={formData.message}
-            onChange={handleChange}
-            className="form-textarea"
-          />
-        </div>
-        <button type="submit" className="form-button">Отправить</button>
-      </form>
+          <div className="form-group">
+            <label className="form-label"><FaComment /> Сообщение</label>
+            <textarea
+              name="message"
+              placeholder="Введите ваше сообщение"
+              value={formData.message}
+              onChange={handleChange}
+              className="form-textarea"
+            />
+          </div>
+          <button type="submit" className="form-button">Отправить</button>
+        </form>
+      </div>
     </div>
   );
 };
